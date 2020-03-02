a2369a9cce34aaf4ece155b12540935acf30ead8
Alexandre LUDWIG <ludwig.alexandre@laposte.net>
2020-03-02 18:10:55 +0100

    :sparkles: trigger for creating user

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,11 @@
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -6,3 +13,18 @@ import * as functions from 'firebase-functions';
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+admin.initializeApp();
+exports.triggerUser = functions.auth.user().onCreate((user) => {
+  console.log("user created");
+  return admin
+    .firestore()
+    .collection('/users/').doc(user.uid).set({
+      email: user.email,
+      firstName:'',
+      lastName:'',
+      phone:'',
+      photo:'',
+      role:'utilisateur',
+      team:'',
+    });
+});
