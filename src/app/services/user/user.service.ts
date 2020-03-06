0fa2f36ca8437b9afa41bd6bf92a9975a9f0c8cc
Alexandre LUDWIG <ludwig.alexandre@laposte.net>
2020-03-06 11:48:01 +0100

    :construction: editUser function

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,40 +1,52 @@
-import { Injectable } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { Observable, BehaviorSubject, of } from 'rxjs';
-import User from '../../model/user.model';
-import {AuthService} from '../auth/auth.service';
-import { switchMap, map, take, tap, first} from 'rxjs/operators';
-import { UserCardComponent } from 'src/app/layout/user-card/user-card.component';
-import { AngularFireAuth } from '@angular/fire/auth';
+import { Injectable } from "@angular/core";
+import { AngularFirestore } from "@angular/fire/firestore";
+import { Observable, BehaviorSubject, of } from "rxjs";
+import User from "../../model/user.model";
+import { AuthService } from "../auth/auth.service";
+import {
+  switchMap,
+  map,
+  take,
+  tap,
+  first,
+  flatMap,
+  reduce
+} from "rxjs/operators";
+import { UserCardComponent } from "src/app/layout/user-card/user-card.component";
+import { AngularFireAuth } from "@angular/fire/auth";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class UserService {
-
-
-  constructor(private afs: AngularFirestore, private authService: AuthService, private afAuth: AngularFireAuth) {
-   }
+  constructor(
+    private afs: AngularFirestore,
+    private authService: AuthService,
+    private afAuth: AngularFireAuth
+  ) {}
 
   public getAllUsers(): Observable<User[]> {
-    return this.afs.collection<User>('users').valueChanges();
+    return this.afs.collection<User>("users").valueChanges();
   }
 
   public getCurrentUser(): Observable<User> {
     // return  this.afs.doc<User>(`users/${this.authService.getCurrentUserId()}`).valueChanges().pipe(take(1));
-    return  this.afs.doc<User>(`users/${this.afAuth.auth.currentUser.uid}`).valueChanges().pipe(take(1));
+    return this.afs
+      .doc<User>(`users/${this.afAuth.auth.currentUser.uid}`)
+      .valueChanges()
+      .pipe(take(1));
   }
 
   public isClient(): Observable<boolean> {
-    return this.getCurrentUser().pipe(map(user => user.role === 'client'));
+    return this.getCurrentUser().pipe(map(user => user.role === "client"));
   }
 
   public isEmploye(): Observable<boolean> {
-    return this.getCurrentUser().pipe(map(user => user.role === 'employe'));
+    return this.getCurrentUser().pipe(map(user => user.role === "employe"));
   }
 
   public isAdmin(): Observable<boolean> {
-    return this.getCurrentUser().pipe(map(user => user.role === 'admin'));
+    return this.getCurrentUser().pipe(map(user => user.role === "admin"));
   }
 
   public getRole(): string {
@@ -43,5 +55,20 @@ export class UserService {
       temp = user;
     });
     return temp.role;
+  }
+
+  public editUser(user: User) {
+    this.afs.firestore
+      .collection("users")
+      .where("email", "==", user.email)
+      .get()
+      .then(snapshot => {
+        snapshot.forEach(doc => {
+          this.afs.firestore
+            .collection("users")
+            .doc(doc.id)
+            .set(user);
+        });
+      });
   }
 }
