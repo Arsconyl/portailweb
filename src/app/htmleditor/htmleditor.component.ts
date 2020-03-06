e5a9245612b3b60f8b8d785b1f432a06ca086511
Alexandre LUDWIG <ludwig.alexandre@laposte.net>
2020-03-06 17:19:18 +0100

    :lipstick: white background color for html editor

--- src/app/htmleditor/htmleditor.component.ts
+++ src/app/htmleditor/htmleditor.component.ts
@@ -1,45 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
-import {UserService} from '../services/user/user.service';
-import User from '../model/user.model';
-import Article from '../model/article.model';
-import {HTMLArticleService} from '../services/htmlArticle/htmlarticle.service';
-import { Observable } from 'rxjs';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl } from "@angular/forms";
+import { UserService } from "../services/user/user.service";
+import User from "../model/user.model";
+import Article from "../model/article.model";
+import { HTMLArticleService } from "../services/htmlArticle/htmlarticle.service";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-htmleditor',
-  templateUrl: './htmleditor.component.html',
-  styleUrls: ['./htmleditor.component.scss']
+  selector: "app-htmleditor",
+  templateUrl: "./htmleditor.component.html",
+  styleUrls: ["./htmleditor.component.scss"]
 })
 export class HtmleditorComponent implements OnInit {
-
-
   editorForm: FormGroup;
   editorStyle = {
-    height: '300px'
+    height: "300px",
+    backgroundColor: "white"
   };
   currentUser: User;
   isAdmin: Observable<boolean>;
 
-  constructor(public userService: UserService, private articleService: HTMLArticleService){}
+  constructor(
+    public userService: UserService,
+    private articleService: HTMLArticleService
+  ) {}
 
   ngOnInit() {
     this.editorForm = new FormGroup({
-      'editor': new FormControl(null),
-      'title': new FormControl(null)
+      editor: new FormControl(null),
+      title: new FormControl(null)
     });
-    this.userService.getCurrentUser().subscribe(user =>{
+    this.userService.getCurrentUser().subscribe(user => {
       this.currentUser = user;
     });
     this.isAdmin = this.userService.isAdmin();
   }
 
-
   onSubmit() {
-    console.log(this.editorForm.get('title').value);
+    console.log(this.editorForm.get("title").value);
     const newArticle: Article = {
-      title: this.editorForm.get('title').value,
-      content: this.editorForm.get('editor').value
+      title: this.editorForm.get("title").value,
+      content: this.editorForm.get("editor").value
     };
 
     this.articleService.createArticle(newArticle);
@@ -48,5 +49,4 @@ export class HtmleditorComponent implements OnInit {
   showInfo() {
     console.log(this.currentUser.role);
   }
-
 }
